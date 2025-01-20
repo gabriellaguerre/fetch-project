97c9960680cbb7772fb4aad5c9032039285650fa
gabriel <gabriel.g.laguerre@gmail.com>
2025-01-20 16:53:28 -0500

    started auth and redux

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Route, Switch, Redirect } from "react-router-dom";
+import LoginPage from "./components/LoginPage";
 
 function App() {
+
+  const dispatch = useDispatch();
+
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    dispatch(authenticate()).then(() => setIsLoaded(true));
+  }, [dispatch]);
+
+  const user = useSelector(state => state.session.user);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello World!
-        </p>
-        </header>
-    </div>
+    <>
+      {isLoaded && (
+          <Switch>
+            <Route path="/login">
+                <LoginPage />
+            </Route>
+            <Route exact path="/">
+                {(user) ? <Redirect to="/" /> : <LoginPage />}
+            </Route>
+            <Route>
+              <h1>404: Page not found</h1>
+            </Route>
+          </Switch>
+        )};
+    </>
   );
 }
 
---
+++ src/components/LoginPage/index.js
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import { login } from "../../store/session";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
+import './LoginPage.css';
+
+
+function LoginPage() {
+  const dispatch = useDispatch();
+  const sessionUser = useSelector((state) => state.session.user);
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [errors, setErrors] = useState([]);
+
+  if (sessionUser) return <Redirect to="/" />;
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = await dispatch(login(employeeID, password));
+    if (data) {
+      setErrors(data);
+    }
+  };
+
+  const Admin = async (e) => {
+    e.preventDefault();
+    await dispatch(login("Demo", "password"))
+    .then(<Redirect to='/items' />)
+  }
+
+  const Employee = async (e) => {
+    e.preventDefault();
+    await dispatch(login("marnie", "password"))
+    .then(<Redirect to='/items' />)
+  }
+
+
+  return (
+    <>
+<div className='allbody'>
+ <form onSubmit={handleSubmit}>
+    <div className='formBodylogin'>
+      <div className='imageLogin'>
+      <img src={ivy3} alt="ivy-pic" width="130" height="100"></img>
+      </div>
+      <div className='titleLogin'>Log In</div>
+        <div className='errorsLogin'>
+        <ul>
+          {errors.map((error, idx) => (
+            <li key={idx} style={{color:'red'}}>{error}</li>
+          ))}
+        </ul>
+        </div>
+        <div className='employeeID-DIV'>
+          Employee ID:{' '}
+          <input
+            type="text"
+            value={employeeID}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className='passwordDiv'>
+          Password:{' '}
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div className='buttons'>
+        <span className='submitLogin'><button id='submitLogin' type="submit">Log In</button></span>
+        <span className='adminLogin'><button id='adminLogin' onClick={Admin}>Demo Admin</button></span>
+        <span className='employeeLogin'><button id='employeeLogin'onClick={Employee}>Demo Employee</button></span>
+    </div>
+    </div>
+    </form>
+    </div>
+    </>
+  );
+}
+
+export default LoginPage;
