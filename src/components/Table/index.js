64fd4bbdd80a36551795e4cd9d2eaabfbe694cd7
Gabriel Laguerre <gabriel.g.laguerre@gmail.com>
2025-01-31 13:07:37 -0500

    started table results

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,12 +1,42 @@
 import React from 'react';
+import './Table.css'
 
-function Table() {
+function Table({searchResult}) {
+    let total = searchResult.total
+    let dogIds = searchResult.resultIds
+    let next = searchResult.next
+    console.log(dogIds, 'inside table function')
 
+    const handleNext = () => {
+        console.log(next, 'next')
+    }
 
   return (
-    <div>
-      Hello
-    </div>
+    <>
+    <div>Total Finds: {total}</div>
+    <div><button onClick={handleNext}>Next</button></div>
+   
+    <table className='resultDisplayed'>
+      <thead>
+        <tr className='labels'>
+            <th>Breed Id</th>
+            <th>Something</th>
+            <th>Something More</th>
+        </tr>
+      </thead>
+      <tbody>
+        {dogIds?.map(item=>
+            <tr key={item.id}>
+                <td>{item}</td>
+                <td>na</td>
+                <td>and</td>
+            </tr>
+        )}
+        
+        
+      </tbody>
+    </table>
+    </>
   )
 }
 
